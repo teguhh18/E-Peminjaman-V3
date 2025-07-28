40a1fc728bd8625b649d8e3a417f05194d1f543b
afifwijaya16 <afif0071997@gmail.com>
2025-07-28 16:12:09 +0700

    chore (user-mahasiswa) : change color

--- resources/views/layouts/tabler-front/header.blade.php
+++ resources/views/layouts/tabler-front/header.blade.php
@@ -12,7 +12,6 @@
             </a>
         </h1>
         <div class="navbar-nav flex-row order-md-last">
-
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
                     aria-label="Open user menu">
@@ -25,7 +24,7 @@
                     @endif
                     <div class="d-none d-xl-block ps-2">
                         <div>{{ auth()->user()->name }}</div>
-                        <div class="mt-1 small text-dark">{{ auth()->user()->username }}</div>
+                        <div class="mt-1 small">{{ auth()->user()->username }}</div>
                     </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
